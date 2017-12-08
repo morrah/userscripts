3b3127e7b911711c4710f54864abfbddf5ac38ff
morrah <morrah@users.noreply.github.com>
2017-12-08 12:02:23 +0300

    автозаполнение формы пациента
    добавлена возможность сохранять содержимое форм для последующего автозаполнения;
    проверка обновлений юзерскрипта;

--- gorzdrav.spb.ru.user.js
+++ gorzdrav.spb.ru.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         doctor checker
+// @name         Doctor Checker
 // @namespace    https://gorzdrav.spb.ru/
-// @version      0.1
-// @description  checks for free applications
+// @version      0.2
+// @description  checks for available applications
 // @author       morrah@neko.im
 // @match        https://gorzdrav.spb.ru/*
+// @downloadURL  https://github.com/morrah/userscripts/raw/master/gorzdrav.spb.ru.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -14,24 +15,64 @@
     function addObserverForm() {
         var frm = `<div id="observerForm">
             <style>
-                #observerForm {
+                #observerForm, #patientHelperForm {
                     background: #aaa;
                     padding: 0.2rem;
+                }
+                #patientHelperAdd {
+                    padding: 0.5rem;
+                    text-align: center;
+                }
+                .patient-helper-button .icon-img {
+                    padding-top: 0.5rem;
+                    padding-bottom: 0.5rem;
+                    padding-left: 1rem;
+                    padding-right: 1rem;
+                }
+                .patient-text {
+                    display: inline-block;
+                    padding: 0.5rem;
+                }
+                .btn {
+                    cursor: pointer;
+                    -webkit-transition-duration: 0.5s;
+                       -moz-transition-duration: 0.5s;
+                         -o-transition-duration: 0.5s;
+                            transition-duration: 0.5s;
+                            user-select: none;
+                       -moz-user-select: none;
+                     -khtml-user-select: none;
+                    -webkit-user-select: none;
+                         -o-user-select: none;
+                }
+                .btn:hover {
+                    background: #FF66CC;
+                }
+                .btn:active {
+                    background: #e738ff;
                 }
                 .icon-img {
                     padding-left: 1rem;
                     padding-right: 1rem;
+                    margin-right: 0.2rem;
                     float: left;
-                }
-                .icon-img:hover {
-                    background: #FF66CC;
-                    cursor: pointer;
                 }
                 .app-found {
                     background: green;
+                    -webkit-transition-duration: 0.5s;
+                       -moz-transition-duration: 0.5s;
+                         -o-transition-duration: 0.5s;
+                            transition-duration: 0.5s;
+                }
+                .app-checking {
+                    background: white;
+                    -webkit-transition-duration: 0.5s;
+                       -moz-transition-duration: 0.5s;
+                         -o-transition-duration: 0.5s;
+                            transition-duration: 0.5s;
                 }
             </style>
-            <div class="icon-img plus-img" title="добавить врачей со страницы">+</div>
+            <div class="icon-img plus-img btn" title="добавить всех врачей со страницы">+</div>
             <div>Список отслеживаемых врачей:</div>
         </div>`;
         var d = document.createElement('div');
@@ -42,25 +83,23 @@
         };
         return document.querySelector('#observerForm');
     }
-    function addDoctor(id, desc) {
-        console.log('adding #'+id+': '+desc);
+    function addDoctor(id, doctorId, clinicId, desc) {
+        console.log('adding #'+id+': clinic #' + clinicId + '; doctor #' + doctorId + '; ' + desc);
         var doctor = `<div id="doctor` + id + `">
-            <div class="icon-img minus-img">–</div>
-            <div>` + id + ` ` + desc + `</div>
+            <div class="icon-img minus-img btn" title="удалить врача из списка">–</div>
+            <div>` + desc + `</div>
         </div>`;
-        observerDoctorList[id] = { 'desc': desc, 'timer': setChecker(id) };
+        observerDoctorList[id] = { 'desc': desc, 'timer': setChecker(id, doctorId, clinicId), 'doctor': doctorId, 'clinic': clinicId };
         var d = document.createElement('div');
         d.innerHTML = doctor;
         observerForm.appendChild(d.childNodes[0]);
         observerForm.querySelector('#doctor'+id+' > .icon-img').onclick = function() {
-            console.log(id);
             removeDoctor(id);
         };
         localStorage.setItem('observerDoctorList', JSON.stringify(observerDoctorList));
         return id;
     }
     function removeDoctor(id) {
-        console.log(id);
         clearTimeout(observerDoctorList[id].timer);
         delete observerDoctorList[id];
         localStorage.setItem('observerDoctorList', JSON.stringify(observerDoctorList));
@@ -73,14 +112,19 @@
         var doctors = document.querySelectorAll('#doctor_list > li');
         doctors.forEach(function(doctor) {
             if (!doctor.querySelector('.icon-img')){
-                var frm = `<div class="icon-img plus-img">+</div>`;
+                var frm = `<div class="icon-img plus-img btn" title="добавить врача в список">+</div>`;
                 var d = document.createElement('div');
                 d.innerHTML = frm;
                 doctor.insertBefore(d.childNodes[0], doctor.childNodes[0]);
                 doctor.querySelector('.plus-img').onclick = function() {
                     event.stopPropagation();
                     if (!(doctor.getAttribute('data-id') in observerDoctorList)) {
-                        addDoctor(doctor.getAttribute('data-id'), doctor.innerText);
+                        addDoctor(
+                            calcHash(doctor.getAttribute('data-id')),
+                            doctor.getAttribute('data-id'),
+                            app.view.forms[1].model.attributes.values.clinic,
+                            doctor.innerText
+                        );
                     }
                 };
             }
@@ -88,18 +132,28 @@
     }
     function addAllDoctors() {
         var doctors = document.querySelectorAll('#doctor_list > li');
+        if ((doctors.length > 0) && doctors[0].classList.contains('empty_elem')) {
+            return;
+        }
         doctors.forEach(function(doctor) {
             if (!(doctor.getAttribute('data-id') in observerDoctorList)) {
-                addDoctor(doctor.getAttribute('data-id'), doctor.innerText);
+                addDoctor(
+                    calcHash(doctor.getAttribute('data-id')),
+                    doctor.getAttribute('data-id'),
+                    app.view.forms[1].model.attributes.values.clinic,
+                    doctor.innerText
+                );
             }
         });
     }
-    function checkDoctor(id) {
+    function checkDoctor(id, doctorId, clinicId) {
         var url = 'https://gorzdrav.spb.ru/api/doctor_schedule/';
         var data = {
-            'doctor_form-doctor_id': id,
-            'doctor_form-clinic_id': app.view.forms[1].model.attributes.values.clinic || '193',
+            'doctor_form-doctor_id': doctorId,
+            'doctor_form-clinic_id': clinicId,
         };
+        document.querySelector('#doctor'+id).classList.remove('app-found');
+        document.querySelector('#doctor'+id).classList.add('app-checking');
         $.post(url, data)
         .done(function(data) {
             var apps = [];
@@ -111,37 +165,135 @@
                 });
             });
             console.log(apps.length);
+            document.querySelector('#doctor'+id).classList.remove('app-checking');
             if (apps.length > 0) {
-                document.querySelector('#doctor'+id).classList.add("app-found");
+                document.querySelector('#doctor'+id).classList.add('app-found');
                 player.play();
             } else {
-                document.querySelector('#doctor'+id).classList.remove("app-found");
+                document.querySelector('#doctor'+id).classList.remove('app-found');
             }
         });
     }
-    function setChecker(id) {
+    function setChecker(id, doctorId, clinicId) {
         var timer = setInterval(function(){
-            console.log('checking #' + id);
-            checkDoctor(id);
-        }, 10000);
+            console.log('checking #' + id + '; doctorId #' + doctorId + '; clinicId #' + clinicId);
+            checkDoctor(id, doctorId, clinicId);
+        }, checkPeriod);
         return timer;
     }
+    function calcHash(s) {
+        var hash = 0,
+            strlen = s.length,
+            i,
+            c;
+        if ( strlen === 0 ) {
+            return hash;
+        }
+        for ( i = 0; i < strlen; i++ ) {
+            c = s.charCodeAt( i );
+            hash = ((hash << 5) - hash) + c;
+            hash = hash & hash; // Convert to 32bit integer
+        }
+        return hash;
+    }
+
+    // patient helper form functions
+    function addPatient() {
+        var patient = {};
+        patientForm.forEach(function(key) {
+            var field = document.querySelector(key);
+            if (field) {
+                patient[key] = field.value;
+            }
+        });
+        var patientId = patient['input#id_patient_form-last_name'] +
+            ' ' +
+            patient['input#id_patient_form-first_name'];
+        if (patientId.trim() === '') {
+            return;
+        }
+        console.log(patientList);
+        patientList[calcHash(patientId)] = Object.assign({}, patientList[calcHash(patientId)], patient);
+        console.log(patientList);
+        localStorage.setItem('patientList', JSON.stringify(patientList));
+        redrawPatientForm();
+    }
+    function removePatient(patientId) {
+        delete patientList[patientId];
+        localStorage.setItem('patientList', JSON.stringify(patientList));
+        redrawPatientForm();
+    }
+    function redrawPatientForm() {
+        var oldForm = document.querySelector('#patientHelperForm');
+        if (oldForm) {
+            oldForm.parentNode.removeChild( oldForm );
+        }
+        var buttonList = '';
+        for (var key in patientList) {
+            if (patientList.hasOwnProperty(key)) {
+                var patientName = patientList[key]['input#id_patient_form-last_name'] +
+                    ' ' +
+                    patientList[key]['input#id_patient_form-first_name'];
+                buttonList += `<div class="patient-helper-button" data-id="` + key + `">
+                               <div class="icon-img minus-img btn" title="Удалить пациента из списка пресетов">–</div>
+                               <div class="patient-text btn">` + patientName + `</div>
+                               </div>`;
+            }
+        }
+        var frm = `<div id="patientHelperForm">
+                   <div id="patientHelperAdd" class="btn">Добавить форму в список пресетов</div>
+                  ` + buttonList + `
+                   </div>`;
+        var d = document.createElement('div');
+        d.innerHTML = frm;
+        document.querySelector('#patient_form .info').append(d.childNodes[0]);
+        var btns = document.querySelectorAll('#patient_form .info .patient-helper-button');
+        btns.forEach(function(btn) {
+            var id = btn.getAttribute('data-id');
+            btn.querySelector('.minus-img').onclick = function() {
+                removePatient(id);
+            };
+            btn.querySelector('.patient-text').onclick = function() {
+                patientForm.forEach(function(key){
+                    var field = document.querySelector(key);
+                    if (field) {
+                        field.value = patientList[id][key];
+                    }
+                });
+            };
+        });
+        document.querySelector('#patientHelperAdd').onclick = function() {
+            addPatient();
+        };
+    }
+
+    var turnOffAllHashchangeTimers = false;
     $(window).bind('hashchange', function() {
         console.log('hashchange fired');
-        // look for doctor-list for 5 seconds
+        // look for doctor-list for 10 seconds
         var counts = 0;
         var timer = setInterval(function() {
             counts++;
-            if (document.querySelector('#doctor_list').innerText.trim() !== '') {
+            var doctorList = document.querySelector('#doctor_list');
+            var patientForm = document.querySelector('#patient_form');
+            if (doctorList && doctorList.innerText.trim() !== '') {
                 clearInterval(timer);
+                turnOffAllHashchangeTimers = true;
                 hookDoctors();
             }
-            if (counts > 10) {
+            if (patientForm && patientForm.innerText.trim() !== '') {
                 clearInterval(timer);
+                turnOffAllHashchangeTimers = true;
+                redrawPatientForm();
+            }
+            if ((counts > 20) || (turnOffAllHashchangeTimers)) {
+                clearInterval(timer);
+                turnOffAllHashchangeTimers = false;
             }
         }, 500);
     });
 
+    var checkPeriod = 10000; // check every 10 seconds
     var player = document.createElement('audio');
     player.src = 'https://files.catbox.moe/qpctri.mp3';
     player.preload = 'auto';
@@ -151,8 +303,21 @@
     for (var key in observerDoctorList) {
         if (observerDoctorList.hasOwnProperty(key)) {
             console.log(key, observerDoctorList[key]);
-            addDoctor(key, observerDoctorList[key].desc);
+            addDoctor(key, observerDoctorList[key].doctor, observerDoctorList[key].clinic, observerDoctorList[key].desc);
         }
     }
+    var patientList = JSON.parse(localStorage.getItem('patientList')) || {};
+    var patientForm = [
+        'input#id_patient_form-last_name',
+        'input#id_patient_form-first_name',
+        'input.jq_input_day',
+        'input.jq_input_month_verbose',
+        'input.jq_input_year',
+        'input.jq_input_month_actual',
+        'input#id_patient_form-insurance_series',
+        'input#id_patient_form-insurance_number',
+        'input#id_patient_form-email',
+        'input#id_patient_form-phone',
+    ];
     $(window).trigger('hashchange');
 })();
